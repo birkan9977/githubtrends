8040a22070d5a6be56522cacb4f07a8e957745bb
Birkan Bilici <birkanmail@gmail.com>
2020-08-15 18:07:04 +0300

    data based filter introduced with object.map

--- src/components/filter.jsx
+++ src/components/filter.jsx
@@ -27,6 +27,21 @@ const sendtoReducer = (filterName,filterValue) => {
   };
   dispatch(action);
 };
+const codelanguages = [
+  'JavaScript','Python','Java','Java','C++','Swift','Ruby','C','C#','Rust',
+  'TypeScript','Dart','Shell','Objective-C','CSS'
+
+]
+
+codelanguages.sort()
+
+
+const starValues = {'0':0,'100':100,'500':500,'1k':1000,'5k':5000,'10k':10000,
+                    '20k':20000,'30k':30000,'40k':40000,'50k':50000,
+                    '100k':100000}
+
+                    //Object.entries(starValues)
+//starValues.sort()
 
   return(
     
@@ -35,35 +50,18 @@ const sendtoReducer = (filterName,filterValue) => {
       <label htmlFor='dropdown-language' id='label-language'>Language</label>
       <select id='dropdown-language' name='dropdown-language' defaultValue='Python'
         onChange={(e) => sendtoReducer('language',e.target.value)}>
-        <option value=''>All</option>
-        <option value='Javascript'>Java Script</option>
-        <option value='Python'>Python</option>
-        <option value='Java'>Java</option>
-        <option value='C++'>C++</option>
-        <option value='Swift'>Swift</option>
-        <option value='Ruby'>Ruby</option>
-        <option value='C'>C</option>
-        <option value='C#'>C#</option>
-        <option value='Rust'>Rust</option>
-        <option value='TypeScript'>TypeScript</option>
-        <option value='Dart'>Dart</option>
-        <option value='Shell'>Shell</option>
-        <option value='Objective-C'>Objective-C</option>
-        <option value='CSS'>CSS</option>
+        {codelanguages.map(language => 
+        <option value={language}>{language}</option>
+        )}
       </select>
      
       <label htmlFor='dropdown-followers' id='label-followers'>Stars greater than:</label>
       <select id='dropdown-followers' defaultValue='10000'
         onChange={(e) => sendtoReducer('stars',e.target.value)}>
-        <option value='0'>0</option>
-        <option value='500'>500</option>
-        <option value='1000'>1000</option>
-        <option value='5000'>5000</option>
-        <option value='10000'>10000</option>
-        <option value='20000'>20000</option>
-        <option value='30000'>30000</option>
-        <option value='40000'>40000</option>
-        <option value='50000'>50000</option>
+        {Object.entries(starValues).sort((a,b)=>a-b).map(([key,value]) =>
+        <option value={value}>{key}</option>
+        )}
+        
       </select>
 
       <label htmlFor='input-keyword' id='label-keyword'>Search Keyword</label>
