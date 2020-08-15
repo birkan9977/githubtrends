846962d0b3d89fdd9b701b67d3260cd92d460255
Birkan Bilici <birkanmail@gmail.com>
2020-08-15 18:53:43 +0300

    filter code page made prettier

--- src/components/filter.jsx
+++ src/components/filter.jsx
@@ -49,32 +49,47 @@ const starValues = {'0':0,'100':100,'500':500,'1k':1000,'5k':5000,'10k':10000,
     
     <div id='filter-queries'>
       <label htmlFor='dropdown-language' id='label-language'>Language</label>
-      <select id='dropdown-language' name='dropdown-language' defaultValue='Python'
-        onChange={(e) => sendtoReducer('language',e.target.value)}>
-        {Object.entries(codelanguages).sort().map(([key,value]) => 
-        <option value={value}>{key}</option>
-        )}
+      
+      <select id='dropdown-language' 
+              name='dropdown-language' 
+              defaultValue='Python'
+              
+              onChange={(e) => sendtoReducer('language',e.target.value)}>
+                
+                {Object.entries(codelanguages).sort().map(([key,value]) => 
+                <option value={value}>{key}</option>
+                )}
+
       </select>
      
       <label htmlFor='dropdown-followers' id='label-followers'>Stars greater than:</label>
-      <select id='dropdown-followers' defaultValue='10000'
-        onChange={(e) => sendtoReducer('stars',e.target.value)}>
-        {Object.entries(starValues).sort((a,b)=>a[1]-b[1]).map(([key,value]) =>
-        <option value={value}>{key}</option>
-        )}
+      
+      <select id='dropdown-followers' 
+              defaultValue='10000'
+              
+              onChange={(e) => sendtoReducer('stars',e.target.value)}>
+
+                {Object.entries(starValues).sort((a,b)=>a[1]-b[1]).map(([key,value]) =>
+                <option value={value}>{key}</option>
+                )}
         
       </select>
 
-      <label htmlFor='input-keyword' id='label-keyword'>Search Keyword</label>
-      <input type='text' id='input-keyword'
-      onChange={handleTextKeyChange}
-      onKeyPress={(e)=>e.which===13?sendtoReducer('keyword',keyword):null}
+      <label  htmlFor='input-keyword' 
+              id='label-keyword'>Search Keyword</label>
+
+      <input  type='text' 
+              id='input-keyword'
+              onChange={handleTextKeyChange}
+              onKeyPress={(e)=>e.which===13?sendtoReducer('keyword',keyword):null}
       >
               {/*console.log(keyword)*/}
       
 
       </input>
-      <button onClick={() => sendtoReducer('keyword',keyword)}>Submit</button>
+      
+      <button 
+              onClick={() => sendtoReducer('keyword',keyword)}>Submit</button>
     </div>
     
     
